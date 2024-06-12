56486e1687f318ac84c5a4ef20107efe0874270e
Danny <dorritydj@gmail.com>
2024-06-12 00:07:22 -0400

    feat: 13.5

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,36 +1,20 @@
 package main
 
 import (
-	"bytes"
-	"io"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 
 	"snippetbox.dorrity.dj/internal/assert"
 )
 
 func TestPing(t *testing.T) {
-	responseRecorder := httptest.NewRecorder()
+	app := newTestApplication(t)
 
-	request, err := http.NewRequest(http.MethodGet, "/", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
 
-	ping(responseRecorder, request)
+	code, _, body := ts.get(t, "/ping")
 
-	result := responseRecorder.Result()
-
-	assert.Equal(t, result.StatusCode, http.StatusOK)
-
-	defer result.Body.Close()
-	body, err := io.ReadAll(result.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	body = bytes.TrimSpace(body)
-
+	assert.Equal(t, code, http.StatusOK)
 	assert.Equal(t, string(body), "OK")
 }
